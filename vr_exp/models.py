6c250d917c59664efc6649b2513fd29e3565766b
Austin Do <austinndo@gmail.com>
2023-06-09 17:51:49 -0700

    Creating models

--- vr_exp/models.py
+++ vr_exp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Experience(models.Model):
+    HEADSETS = [
+        ("Oculus Rift", "Oculus Rift"),
+        ("HTC Vive", "HTC Vive"),
+        ("PlayStation VR", "PlayStation VR"),
+        ("Meta Quest", "Meta Quest"),
+        ("Other", "Other")]
+    MOTION_SICKNESS_CHOICES = [(i, i) for i in range(1, 11)]
+    IMMERSION_LEVEL_CHOICES = [(i, i) for i in range(1, 6)]
+
+    user_age = models.PositiveIntegerField(
+        validators=[MinValueValidator(18)])
+    headset_type = models.CharField(choices=HEADSETS)
+    duration_of_use = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)])
+    motion_sickness = models.PositiveIntegerField(
+        choices=MOTION_SICKNESS_CHOICES)
+    immersion_level = models.PositiveIntegerField(
+        choices=IMMERSION_LEVEL_CHOICES)
+
+    def __str__(self):
+        return self.name
