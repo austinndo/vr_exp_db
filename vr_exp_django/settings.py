f2c0e606d4c495ce288ab285ac7697ad98cfb7b4
Austin Do <austinndo@gmail.com>
2023-06-09 17:14:55 -0700

    Updated DATABASES in settings.py

--- vr_exp_django/settings.py
+++ vr_exp_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'vr_exp_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vr_exp_db',
+        'USER': 'vr_exp_admin',
+        'PASSWORD': 'virtual',
+        'HOST': 'localhost'
     }
 }
 
