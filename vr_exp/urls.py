74d06f7fddc747ce4c3aec731f07740a386a8792
Austin Do <austinndo@gmail.com>
2023-06-12 14:23:07 -0700

    Urls updated. should be done with 2/3 for setting up django rest framework

--- vr_exp/urls.py
+++ vr_exp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('', views.experiences, name='experiences')
+    path('', views.experiences, name='experiences'),
+    path('experiences/', views.ExperienceList.as_view(), name='experience_list'),
+    path('experiences/<int:pk>', views.ExperienceDetail.as_view(),
+         name='experience_detail')
 ]
